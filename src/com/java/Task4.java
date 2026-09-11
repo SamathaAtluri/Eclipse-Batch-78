e12ba6167e2feaa56c53e65d81ce9381a5a21455
Atluri Samatha <samathaatluri974@gmail.com>
2026-09-11 10:53:08 +0530

    Task4

---
+++ src/com/java/Task4.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class Task4 {
+	
+	public static void main(String[] args) {
+		
+		int Money =100;
+		int Cost =7;
+		
+		int Pencils = Money / Cost;
+		int Remaining = Money % Cost;
+		
+		System.out.println("No of Pencils:"+Pencils);
+		System.out.println("Remaining Amount:"+Remaining);
+		
+		
+
+	}
+
+}
